3eb980b22a9d56cd9f0ba20f22f2c7d8a64dcd01
Rob Antonisse <robantonisse@gmail.com>
2022-05-15 11:16:31 +0200

    shift works

--- OverWeg/OverWeg/OverWeg.ino
+++ OverWeg/OverWeg/OverWeg.ino
@@ -8,12 +8,48 @@
 
 */
 
-// the setup function runs once when you press reset or power the board
+
+
+//byte sb; //shiftbyte GPIOR0 (gaat ff sneller)
+ # define sb GPIOR0 //gebruik ingebakken register
+//bit0 led0; bit1 led1; bit2 led2; bit3 sensL; bit4 sensR ;bit5 switch;bit6 servo1;bit7 servo2
+
+//temp
+unsigned long tijd;
+
+
 void setup() {
 
+	//ports
+	DDRB |= (1 << 0); //PB0 pin5 as output SERial
+	DDRB |= (1 << 1); //PB1 pin 6 as output RCLK (latch)
+	DDRB |= (1 << 2); //PB2 pin7 as output SRCLK (shiftclock)
+
+
+	//temp
+	DDRB |= (1 << 3); //pin2 as output
+}
+void Shift() {
+	//shifts sb to external shiftregister
+	for (byte i = 7; i < 8; i--) {
+		if (sb & (1 << i)) {
+			PORTB |= (1 << 0); //serial pin high
+		}
+		else {
+			PORTB &= ~(1 << 0); //serial pin low
+		}
+		PINB |= (1 << 2); PINB |= (1 << 2); //make SRCLK shift puls
+	}
+	PINB |= (1 << 1); PINB |= (1 << 1); //make RCLK puls latch to ouputs
 }
 
-// the loop function runs over and over again until power down or reset
 void loop() {
   
+	if (millis() - tijd > 500) {
+		tijd = millis();
+		//temp
+		sb ^= (1 << 1); //knipper op pin 1 van shiftregister
+		Shift();
+	}
+
 }
